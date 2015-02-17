a44c10423163f847cde0a940bc3a122a39ff1ed9
Tim Schindler <tim@giantswarm.io>
2015-02-17 18:26:58 +0000

    added IsErrBreakerError

--- breaker/error.go
+++ breaker/error.go
@@ -12,20 +12,32 @@ var (
 
 	Mask = errgo.MaskFunc(
 		errgo.Any,
-		IsErrMaxConcurrentReached,
-		IsErrMaxErrorRateReached,
-		IsErrMaxPerformanceLossReached,
+		IsErrBreakerError,
 	)
 )
 
-func IsErrMaxConcurrentReached(err error) bool {
+// IsErrBreakerError checks whether the given error is caused by the breaker or
+// not.
+func IsErrBreakerError(err error) bool {
+	return IsErrMaxConcurrentActionsReached(err) ||
+		IsErrMaxErrorRateReached(err) ||
+		IsErrMaxPerformanceLossReached(err)
+}
+
+// IsErrMaxConcurrentActionsReached checks whether the given error is caused by
+// the MaxConcurrentActions configuration or not.
+func IsErrMaxConcurrentActionsReached(err error) bool {
 	return errgo.Cause(err) == ErrMaxConcurrentActionsReached
 }
 
+// IsErrMaxErrorRateReached checks whether the given error is caused by the
+// MaxErrorRate configuration or not.
 func IsErrMaxErrorRateReached(err error) bool {
 	return errgo.Cause(err) == ErrMaxErrorRateReached
 }
 
+// IsErrMaxPerformanceLossReached checks whether the given error is caused by
+// the MaxPerformanceLoss configuration or not.
 func IsErrMaxPerformanceLossReached(err error) bool {
 	return errgo.Cause(err) == ErrMaxPerformanceLossReached
 }
