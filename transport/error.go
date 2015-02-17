039a5d5641b6e0c44df92f2bf6770366ae9e2a9e
Tim Schindler <tim@giantswarm.io>
2015-02-17 19:05:19 +0000

    error cleanup

--- transport/error.go
+++ transport/error.go
@@ -28,51 +28,90 @@ func IsErrConnectTimeout(err error) bool {
 }
 
 func IsErrRequestTimeout(err error) bool {
-	errCause := errgo.Cause(err)
-
-	if urlErr, ok := errCause.(*url.Error); ok {
-		return errgo.Cause(urlErr.Err) == ErrRequestTimeout
+	if urlErr, ok := err.(*url.Error); ok {
+		if isCustomErrErrRequestTimeout(urlErr.Err) {
+			return true
+		}
 	}
 
-	return errCause == ErrRequestTimeout
+	if isCustomErrErrRequestTimeout(err) {
+		return true
+	}
+
+	return false
 }
 
 func IsErrConnectRefused(err error) bool {
-	errCause := errgo.Cause(err)
-
-	if urlErr, ok := errCause.(*url.Error); ok {
-		urlErrCause := errgo.Cause(urlErr.Err)
-
-		if urlErrCause == ErrConnectRefused {
-			return true
-		}
-
-		if opErr, ok := urlErrCause.(*net.OpError); ok {
-			if errno, ok := opErr.Err.(syscall.Errno); ok {
-				if errno == syscall.ECONNREFUSED {
-					return true
-				}
-			}
-		}
+	if isUrlErrErrConnectRefused(err) {
+		return true
 	}
 
-	if opErr, ok := errCause.(*net.OpError); ok {
-		opErrCause := errgo.Cause(opErr.Err)
-
-		if opErrCause == ErrConnectRefused {
-			return true
-		}
-
-		if errno, ok := opErrCause.(syscall.Errno); ok {
-			if errno == syscall.ECONNREFUSED {
-				return true
-			}
-		}
+	if isOpErrErrConnectRefused(err) {
+		return true
 	}
 
-	return errCause == ErrConnectRefused
+	if isCustomErrErrConnectRefused(err) {
+		return true
+	}
+
+	return false
 }
 
 func IsErrStatusCode5XX(err error) bool {
 	return errgo.Cause(err) == ErrStatusCode5XX
+}
+
+//------------------------------------------------------------------------------
+// private
+
+func isUrlErrErrConnectRefused(err error) bool {
+	errCause := errgo.Cause(err)
+
+	if urlErr, ok := errCause.(*url.Error); ok {
+		if isOpErrErrConnectRefused(urlErr.Err) {
+			return true
+		}
+
+		if isCustomErrErrConnectRefused(urlErr.Err) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isOpErrErrConnectRefused(err error) bool {
+	errCause := errgo.Cause(err)
+
+	if opErr, ok := errCause.(*net.OpError); ok {
+		if isCustomErrErrConnectRefused(opErr.Err) {
+			return true
+		}
+
+		if isSyscallErrErrConnectRefused(opErr.Err) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isSyscallErrErrConnectRefused(err error) bool {
+	errCause := errgo.Cause(err)
+
+	if errno, ok := errCause.(syscall.Errno); ok {
+		if errno == syscall.ECONNREFUSED {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isCustomErrErrConnectRefused(err error) bool {
+	return errgo.Cause(err) == ErrStatusCode5XX
+}
+
+func isCustomErrErrRequestTimeout(err error) bool {
+	return errgo.Cause(err) == ErrRequestTimeout
 }
